d0a6cc653e0e8d4d908337c370932395094805da
freewind <nowindlee@gmail.com>
2019-03-23 23:07:50 +0800

    demo

--- hello.test.ts
+++ hello.test.ts
@@ -1,10 +1,11 @@
-import {forEach} from './hello';
-
 test('forEach mock', () => {
-  const callback = jest.fn(x => x + 100)
-  forEach([1, 2], callback);
+  const myMock = jest.fn()
+  myMock.mockReturnValueOnce(1)
+    .mockReturnValueOnce('a')
+    .mockReturnValue({hello: 'jest'});
 
-  expect(callback.mock.calls.length).toBe(2);
-  expect(callback.mock.calls[0][0]).toBe(1);
-  expect(callback.mock.results[0].value).toBe(101);
+  expect(myMock()).toBe(1);
+  expect(myMock()).toEqual('a');
+  expect(myMock()).toEqual({hello: 'jest'});
+  expect(myMock()).toEqual({hello: 'jest'});
 })
